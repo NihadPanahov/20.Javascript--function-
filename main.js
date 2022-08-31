a69ccde55796a3ea6f24e4a5be02bf1b83accc2f
NihadPanahov <nihadpanah03@gmail.com>
2022-08-31 22:18:11 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// function addNumbers (n1 , n2) {
+//     let result = n1 + n2;
+//     return result;
+// }
+
+// let number1 = 6
+// let number2 = 5
+
+// let output = addNumbers(number1,number2);
+// console.log( 'The sum is ' + output);
+
+// ------
+
+// function greet() {
+//     console.log("Hello there");
+// }
+
+// greet ();
+
+// ------
+
+// function greet(name) {
+//     console.log("Hello " + name);
+// }
+
+// let name = prompt("Enter a name: ")
+
+// greet(name);
+
+// ------
+
+// function toplama (a,b) {
+//     console.log(a + b);
+// }
+
+// toplama(19,25);
+
+
